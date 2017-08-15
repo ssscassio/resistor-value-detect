b412d0d16cdcf953e7808bf4a770d4a6d740906f
Cássio Santos <sss.cassio@gmail.com>
2017-08-15 01:33:14 -0300

    Add Calculation of Color and Precision (Without Tests)

---
+++ main.cpp
@@ -0,0 +1,357 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+const int MIN_OBJECT_AREA = 10000;
+
+struct rangeHSV{
+    int iLowH;  // menor hue
+    int iHighH; // maior hue
+    int iLowS;  // menor saturation
+    int iHighS; // maior saturation
+    int iLowV;  // menor value
+    int iHighV; // maior value
+};
+
+struct colorFounded {
+    double posX;
+    double posY;
+    double area,
+    string color;
+};
+
+enum color_code {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    purple,
+    gray,
+    white,
+    golden,
+    silver,
+};
+
+color_code hashit (std::string const& inString) {
+    if (inString == "purple")   return purple;
+    if (inString == "black")    return black;
+    if (inString == "brown")    return brown;
+    if (inString == "red")      return red;
+    if (inString == "orange")   return orange;
+    if (inString == "yellow")   return yellow;
+    if (inString == "green")    return green;
+    if (inString == "blue")     return blue;
+    if (inString == "gray")     return gray;
+    if (inString == "white")    return white;
+    if (inString == "golden")   return golden;
+    if (inString == "silver")   return silver;
+};
+
+struct rangeHSV  getColorRange(string color){
+    struct rangeHSV HsvLowAndHigh;
+
+    switch(hashit(color)){
+        case black:
+            // Defining black color
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 179;
+            HsvLowAndHigh.iLowS  = 0;
+            HsvLowAndHigh.iHighS = 121;
+            HsvLowAndHigh.iLowV  = 0;
+            HsvLowAndHigh.iHighV = 57;
+            break;
+        case brown:
+            // Defining brown color 
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 85;
+            HsvLowAndHigh.iLowS  = 180;
+            HsvLowAndHigh.iHighS = 249;
+            HsvLowAndHigh.iLowV  = 120;
+            HsvLowAndHigh.iHighV = 211;
+            break;
+        case red:
+            // Defining red color
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 4;
+            HsvLowAndHigh.iLowS  = 124;
+            HsvLowAndHigh.iHighS = 255;
+            HsvLowAndHigh.iLowV  = 74;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case orange:
+            // Defining orange color
+            HsvLowAndHigh.iLowH  = 4;
+            HsvLowAndHigh.iHighH = 22;
+            HsvLowAndHigh.iLowS  = 0;
+            HsvLowAndHigh.iHighS = 255;
+            HsvLowAndHigh.iLowV  = 167;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case yellow:
+            // Defining yellow color
+            HsvLowAndHigh.iLowH  = 10;
+            HsvLowAndHigh.iHighH = 73;
+            HsvLowAndHigh.iLowS  = 250;
+            HsvLowAndHigh.iHighS = 255;
+            HsvLowAndHigh.iLowV  = 225;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case green:
+            // Defining green color
+            HsvLowAndHigh.iLowH  = 19;
+            HsvLowAndHigh.iHighH = 165;
+            HsvLowAndHigh.iLowS  = 151;
+            HsvLowAndHigh.iHighS = 255;
+            HsvLowAndHigh.iLowV  = 165;
+            HsvLowAndHigh.iHighV = 194;
+            break;
+        case blue:
+            // Defining blue color
+            HsvLowAndHigh.iLowH  = 74;
+            HsvLowAndHigh.iHighH = 149;
+            HsvLowAndHigh.iLowS  = 111;
+            HsvLowAndHigh.iHighS = 255;
+            HsvLowAndHigh.iLowV  = 86;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case purple:
+            // Defining purple color
+            HsvLowAndHigh.iLowH  = 121;
+            HsvLowAndHigh.iHighH = 150;
+            HsvLowAndHigh.iLowS  = 159;
+            HsvLowAndHigh.iHighS = 194;
+            HsvLowAndHigh.iLowV  = 97;
+            HsvLowAndHigh.iHighV = 164;
+            break;
+        case gray:
+            // Defining gray color
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 80;
+            HsvLowAndHigh.iLowS  = 0;
+            HsvLowAndHigh.iHighS = 80;
+            HsvLowAndHigh.iLowV  = 34;
+            HsvLowAndHigh.iHighV = 139;
+            break;
+        case white:
+            // Defining white color
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 142;
+            HsvLowAndHigh.iLowS  = 0;
+            HsvLowAndHigh.iHighS = 88;
+            HsvLowAndHigh.iLowV  = 231;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case golden:
+            // Defining golden color
+            HsvLowAndHigh.iLowH  = 18;
+            HsvLowAndHigh.iHighH = 77;
+            HsvLowAndHigh.iLowS  = 93;
+            HsvLowAndHigh.iHighS = 238;
+            HsvLowAndHigh.iLowV  = 81;
+            HsvLowAndHigh.iHighV = 255;
+            break;
+        case silver:
+            // Defining silver color
+            HsvLowAndHigh.iLowH  = 0;
+            HsvLowAndHigh.iHighH = 161;
+            HsvLowAndHigh.iLowS  = 45;
+            HsvLowAndHigh.iHighS = 123;
+            HsvLowAndHigh.iLowV  = 44;
+            HsvLowAndHigh.iHighV = 206;
+    }
+    return HsvHighAndLowValues;
+};
+
+
+Scalar getScalar(string color, int HIGH){
+    struct rangeHSV rangeColor = getColorRange(string color);
+
+    if(HIGH){
+        return Scalar(rangeColor.iHighH,rangeColor.iHighS,rangeColor.iHighV);        
+    }else {
+        return Scalar(rangeColor.iLowH,rangeColor.iLowS,rangeColor.iLowV);
+    }
+};
+
+//Dilatação e Erosão da imagem
+void morphOps(Mat &thresh){
+    Mat erodeElement = getStructuringElement( MORPH_RECT,Size(5,5));
+    Mat dilateElement = getStructuringElement( MORPH_RECT,Size(5,5));
+
+    erode(thresh,thresh,erodeElement);
+    dilate(thresh,thresh,dilateElement);
+
+    erode(thresh,thresh,erodeElement);
+    dilate(thresh,thresh,dilateElement);
+}
+
+struct colorFounded getXYColorPosition (string color, Mat imgHSV) {
+    Mat imgThresholded;
+    inRange(imgHSV, getScalar(color, 0) , getScalar(color, 1) , imgThresholded);
+
+    //Aplicando Dilatação e Erosão na imagem
+    morphOps(threshold);
+
+    Moments oMoments = moments(imgThresholded);
+
+    double dM01 = oMoments.m01;
+    double dM10 = oMoments.m10;
+    double dArea = oMoments.m00;
+    
+    struct colorFounded found;
+    found = {dM10 / dArea, dM01 / dArea, dArea, color};
+    if (dArea > MIN_OBJECT_AREA)
+        return found;
+    return false;
+}
+
+bool orderByXPos(const Data& x, const Data& y) { return x.posX < y.posX; }
+
+bool orderByYPos(const Data& x, const Data& y) { return x.posY < y.posY; }
+
+double getBandValue(double lastValue, int index, string color, int size){
+    
+    if(index == 0){
+        switch(hashit(color)){
+            case golden:
+                return lastValue;
+            case silver:
+                return lastValue;
+            default:
+                if(size == 3 || size == 4) return lastValue + hashit(color)*10;
+                else return lastValue + hashit(color)*100;
+                break;
+        }
+    } else if (index == 1){
+        switch(hashit(color)){
+            case golden:
+                return lastValue;
+            case silver:
+                return  lastValue;
+            default:
+                if(size == 3 || size == 4) return lastValue + hashit(color);
+                else return lastValue + hashit(color)*10;
+                break;
+        }
+    } else if (index == 2){
+        switch(hashit(color)){
+            case golden:
+                if(size == 3 || size == 4)  return lastValue * 0.1;
+                else return lastValue;
+            case silver:
+                if(size == 3 || size == 4) return lastValue * 0.01;
+                else return lastValue;
+            default:
+                if(size == 3 || size == 4) return lastValue * pow(10,hashit(color));
+                else return lastValue + hashit(color);
+                break;
+        }
+    } else if (index == 3){
+        if(size == 3 || size == 4)  return lastValue;
+        else {
+            switch(hashit(color)){
+                case golden:
+                    return lastValue * 0.1;
+                case silver:
+                    return lastValue * 0.01;
+                default:
+                    else return lastValue * pow(10,hashit(color);
+                    break;
+            }
+        } 
+    }
+}
+
+//Efetua somatório do valor do resistor baseado no indice do vetor e a cor em questão
+double getResistorValue(vector<colorFounded> bandFounded) {
+    double value = 0;
+    for (int index = 0, size = bandFounded.size(); index < size; ++index)
+            bandFounded[index].color;
+            value = getBandValue(value, index, color, size );
+	}
+
+    return value;
+}
+
+string getBandPrecision(string color ,  int resistorType) {
+    switch(resistorType) {
+        case 4: case 5: case 6:
+            switch(hashit(color)){
+                case black:  return "";
+                case brown:  return "+/- 1\%";
+                case red:    return "+/- 2\%";
+                case orange: return "";
+                case yellow: return "";
+                case green:  return "+/- 0.5\%";
+                case blue:   return "+/- 0.25\%";
+                case purple: return "+/- 0.1%\%";
+                case gray:   return "+/- 0.05\%";
+                case white:  return "";
+                case golden: return "+/- 5\%";
+                case silver: return "+/- 10\%";
+                default: return "+/- 20\%";
+            }
+        default: return "+/- 20\%";
+    }
+
+}
+
+string getResistorPrecision(vector<colorFounded> bandFounded) {
+    int indexOfPrecision = 0;
+    //Descobrindo o indice da faixa de precisão dependendo do tipo do resistor
+    switch(bandFounded.size()){
+        case 3: indexOfPrecision = -1;  break;
+        case 4: indexOfPrecision = 3;   break;
+        case 5: indexOfPrecision = 4;   break;
+        case 6: indexOfPrecision = 4;   break;
+    }
+    return getBandPrecision(bandFounded[indexOfPrecision].color , bandFounded.size());
+}
+
+int main( int argc, char** argv ){
+    //Declarando Vetor com cores validas para resistor de seis faixas
+    vector<std::string> sixBandColors = {
+        "purple",
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "gray",
+        "white",
+        "golden",
+        "silver",
+    };
+
+    vector<colorFounded> bandFounded;
+
+    //Carregando Imagem
+    Mat imgOriginal;
+    imgOriginal = imread( "resistor.jpg", 1 );
+
+    //Criando uma imagem preta do tamanho da imagem original
+    Mat points = Mat::zeros( imgOriginal.size(), CV_8UC3 );;
+    
+    //Convertendo Imagem de BGR para HSV
+    Mat imgHSV;
+    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+
+    //Encontrando os pontos centrais das cores do resistor
+    for (string& color : sixBandColors){
+        bandFounded.push_back(getXYColorPosition(color,imgHSV));
+	}
+
+    //Ordenando Quanto a posição X dos pontos encontrados
+    sort(bandFounded.begin(), bandFounded.end(), orderByXPos);  
+
+    cout << "Resistor Value" << getResistorValue(bandFounded) << " " << getResistorPrecision(bandFounded) << endln;
+
+}
